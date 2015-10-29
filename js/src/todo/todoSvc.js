bfbc808e2f4576cfba5f090785d8baf187d732c1
Mark Savage <mark@savagebits.com>
2015-10-29 16:32:30 -0500

    working db connection through service

--- js/src/todo/todoSvc.js
+++ js/src/todo/todoSvc.js
@@ -5,13 +5,13 @@ angular.module('app')
 
             },
             getTodos: function() {
-                alert('hello');
-                //var fbConnectionString = 'https://shining-inferno-6516.firebaseio.com/';
-                //var myDataRef = new Firebase(fbConnectionString + 'tasks');
-                //
-                //myDataRef.orderByChild("done").equalTo(false).on("child_added", function(task) {
-                //    alert(task);
-                //});
+                //alert('hello');
+                var fbConnectionString = 'https://shining-inferno-6516.firebaseio.com/';
+                var myDataRef = new Firebase(fbConnectionString + 'tasks');
+
+                myDataRef.orderByChild("done").equalTo(false).on("child_added", function(task) {
+                    alert(task);
+                });
             }
         }
     });
