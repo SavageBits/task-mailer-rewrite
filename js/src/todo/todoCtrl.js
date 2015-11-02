94bb2ccaad0325c625cea87128eb904e2c0fe6e8
Mark Savage <mark@savagebits.com>
2015-11-02 16:38:22 -0600

    working controller and service. about to remove commented code

--- js/src/todo/todoCtrl.js
+++ js/src/todo/todoCtrl.js
@@ -1,11 +1,43 @@
-angular.module('app')
-    .controller('todoCtrl', function($scope, todoSvc) {
-        $scope.doIt = function() {
-            //alert('hello');
-            //var todoSvc = angular.module('app').service('todoSvc');
+//angular.module('app')
+//    .controller('todoCtrl', function($scope, todoSvc) {
+//        $scope.doIt = function() {
+//            //alert('hello');
+//            //var todoSvc = angular.module('app').service('todoSvc');
+//
+//            //console.log(todoSvc);
+//
+//            //todos = todoSvc.getTodos();
+//        };
+//
+//        //$scope.todos = function() {
+//        //    alert('yiss');
+//        //    return todoSvc.getTodos();
+//        //};
+//    });
 
-            //console.log(todoSvc);
 
-            todoSvc.getTodos();
-        }
-    });
+// https://github.com/toddmotto/angularjs-styleguide
+function TodoCtrl(todoSvc) {
+    var vm = this; // capture context as vm which stands for View Model
+
+    //this.doIt = function() {
+    //    alert('hey');
+    //}
+
+    //vm.todos = ['a', 'b'];
+    vm.data = todoSvc.loadTodos();
+    vm.todos =  todoSvc.getTodos();
+    //vm.data = todoSvc.getData();
+
+
+    var doIt = function() {
+        alert('hey');
+    };
+
+    vm.doIt = doIt;
+}
+
+angular
+    .module('app')
+    .controller('TodoCtrl', TodoCtrl);
+
--- js/src/todo/todoSvc.js
+++ js/src/todo/todoSvc.js
@@ -1,17 +1,44 @@
+//this is good but we're using firebase - http://stackoverflow.com/questions/21989300/calling-service-for-factory-in-controller
+
 angular.module('app')
-    .factory('todoSvc', function($firebaseObject) {
-        return {
-            addTodo: function() {
+    .factory('todoSvc', function($http, $firebaseObject) {
+        //var self = this;
+        var TodoSvc = {};
 
-            },
-            getTodos: function() {
-                //alert('hello');
-                var fbConnectionString = 'https://shining-inferno-6516.firebaseio.com/';
-                var myDataRef = new Firebase(fbConnectionString + 'tasks');
+        TodoSvc.todos = [];
 
-                myDataRef.orderByChild("done").equalTo(false).on("child_added", function(task) {
-                    alert(task);
-                });
-            }
-        }
+        TodoSvc.addTodo = function(task) {
+            this.todos.push({
+                key: task.key(),
+                text: task.val().description,
+                //date: taskDate.toDateString(),
+                done: task.val().done
+            })
+        };
+
+        TodoSvc.getTodos = function() {
+          return this.todos;
+        };
+
+        TodoSvc.loadTodos = function() {
+            //alert('hello');
+            var self = this;
+
+            var fbConnectionString = 'https://shining-inferno-6516.firebaseio.com/';
+            var myDataRef = new Firebase(fbConnectionString + 'tasks');
+
+            myDataRef.orderByChild("done").equalTo(false).on("child_added", function(task) {
+                self.addTodo(task);
+
+                console.log(task.val().description);
+            });
+
+            //return self.todos;
+
+            return  $firebaseObject(myDataRef);
+
+//                console.log(self.todos);
+        };
+
+        return TodoSvc;
     });
