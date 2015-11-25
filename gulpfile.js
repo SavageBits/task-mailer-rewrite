e35ec543a54c48ddb85323b3227cd36cb26ff6d2
Mark Savage <mark@savagebits.com>
2015-11-25 13:54:12 -0600

    added gulp for build, watch, and reload

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+
+var config = {
+    port: 9006,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: 'index.html',
+        js: './src/**/*.js'
+    },
+    browser: 'firefox'
+};
+
+gulp.task('connect', function() {
+    connect.server({
+        root: ['.'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    })
+});
+
+gulp.task('open', ['connect'], function() {
+    gulp.src('')
+        .pipe(open({
+            app: config.browser,
+            uri: config.devBaseUrl + ':' + config.port + '/'
+        }))
+});
+
+gulp.task('html', function() {
+    gulp.src(config.paths.html)
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    gulp.src(config.paths.js)
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['open', 'watch']);
